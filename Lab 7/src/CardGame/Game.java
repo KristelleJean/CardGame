8e68e76378dfc5b564b7eaba38c5d3a415e0ae93
KristelleJean <lucerok1@RobiLab303-1.rowanads.rowan.edu>
2015-12-14 17:12:48 -0500

    Debug Complete
    ran through code to determine why sumAndProduct method wasn't working.
    Also determined that the aceValue method runs twice during game play
    because it is run once to determine the sum of the hand, and again to
    determine the product

--- Lab 7/src/CardGame/Game.java
+++ Lab 7/src/CardGame/Game.java
@@ -1,5 +1,38 @@
 package CardGame;
 
+/* ********************************************************************
+* Name: Eric Segally, Kristelle Lucero
+* Class: IOOP
+* Date: 14 December 2015
+* **********************************************************************
+* ID: Lab #07
+* Purpose: Using a standard deck of cards, develop a game for two 
+* players that start the players with 0 cards. At the start of the 
+* game, a number between 10 and 50 is randomly generated as the target 
+* for this game. On each turn the player can determine whether they 
+* would like to pick up a card from the deck or drop a card. The player 
+* can have a maximum of 5 cards in their hand at any time. The goal is 
+* for the player to hold a collection of cards that reaches the target 
+* through sum or product. The target cannot be reached by the sum or 
+* product of a subset of the player's cards. For a player's turn, if 
+* they choose to draw a card the player adds the first card from the 
+* deck to their hand and their turn is complete. The top card on the 
+* deck is hidden, so the player does not know what card they will be 
+* picking up before they do. If the player drops a card for their turn, 
+* you must ask the player which card they would like to drop from their 
+* hand and their hand is updated accordingly. Once one player achieves 
+* the target, the other player has one more turn. If the other player 
+* achieves the target in the final turn, display that the game results 
+* in a tie. Otherwise, display which player has won and their hand. At 
+* any time if the deck becomes empty, the collection of dropped cards 
+* is shuffled and replaces the empty deck. Normal rules apply: a non 
+* face card gets the value of the card, a face card is 10, and an Ace 
+* is 1 or 11 (you will have to ask the user which they would like it 
+* to be).
+* *********************************************************************
+*/
+package CardGame;
+
 import java.util.Scanner;
 
 public class Game {
@@ -7,6 +40,7 @@ public class Game {
 	public static void main(String[] args){
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
+		boolean noWinner = true, p1Result = false, p2Result = false;
 		
 		//Ask for players name.
 		System.out.println("Enter the first players name!");
@@ -24,11 +58,25 @@ public class Game {
 		do{
 			player_one.takeTurn();
 			player_two.takeTurn();
+			p1Result = player_one.completeTurn();
+			p2Result = player_two.completeTurn();
 			
-		}while(player_one.completeTurn());
+			if(p1Result){
+				noWinner= false;
+			}else if (p2Result){
+				noWinner= false;
+			}
+		}while(noWinner);
 		
+		System.out.println("Player one's hand:");
 		player_one.displayHand();
+		System.out.println("\nPlayer two's hand:");
 		player_two.displayHand();
+		if(p1Result){
+			System.out.println("\nPlayer one wins!");
+		}else if (p2Result){
+			System.out.println("\nPlayer one wins!");
+		}
 		
 		System.out.println("End of the Game!");
 	}
--- Lab 7/src/CardGame/Hand.java
+++ Lab 7/src/CardGame/Hand.java
@@ -1,34 +1,16 @@
 package CardGame;
 
 /* *********************************************************************
-* Name: 
+* Name: Eric Segally, Kristelle Lucero
 * Class: IOOP
 * Date: 14 December 2015
 * **********************************************************************
 * ID: Lab #07
-* Purpose: Using a standard deck of cards, develop a game for two 
-* players that start the players with 0 cards. At the start of the 
-* game, a number between 10 and 50 is randomly generated as the target 
-* for this game. On each turn the player can determine whether they 
-* would like to pick up a card from the deck or drop a card. The player 
-* can have a maximum of 5 cards in their hand at any time. The goal is 
-* for the player to hold a collection of cards that reaches the target 
-* through sum or product. The target cannot be reached by the sum or 
-* product of a subset of the player's cards. For a player's turn, if 
-* they choose to draw a card the player adds the first card from the 
-* deck to their hand and their turn is complete. The top card on the 
-* deck is hidden, so the player does not know what card they will be 
-* picking up before they do. If the player drops a card for their turn, 
-* you must ask the player which card they would like to drop from their 
-* hand and their hand is updated accordingly. Once one player achieves 
-* the target, the other player has one more turn. If the other player 
-* achieves the target in the final turn, display that the game results 
-* in a tie. Otherwise, display which player has won and their hand. At 
-* any time if the deck becomes empty, the collection of dropped cards 
-* is shuffled and replaces the empty deck. Normal rules apply: a non 
-* face card gets the value of the card, a face card is 10, and an Ace 
-* is 1 or 11 (you will have to ask the user which they would like it 
-* to be).
+* Purpose: Simluates the hand of a player in the cardgame. The hand may
+* contain a max of 5 cards and a min of 1 card. Players may choose to 
+* draw from the deck and add to their current hand or discard a card 
+* of their choice. The sum of each card in the hand must be determined
+* along with the product of each card in the hand.
 * *********************************************************************
 * TEST SUITE
 * *********************************************************************
@@ -132,11 +114,11 @@ public class Hand {
 		//calculate the product
 		for (int i = 0; i < hand.size(); i++) {
 			if (hand.get(i).equals("A")) {
-				sum *= aceValue();
+				product *= aceValue();
 			} else if (hand.get(i).equals("J")||hand.get(i).equals("Q")||hand.get(i).equals("K")) {
-				sum *= 10;
+				product *= 10;
 			} else {
-				sum *= Integer.parseInt(hand.get(i));
+				product *= Integer.parseInt(hand.get(i));
 			}
 			result[1]=product;
 		}
